54a1d18d56e18131a005fab4f7a9c79187795d57
pikk7 <durza13@gmail.com>
2023-07-24 15:33:41 +0200

    Input handling without promeses,with bad animation

--- Grid.js
+++ Grid.js
@@ -4,6 +4,7 @@ const CELL_GAP = 2;
 
 export default class Grid {
   #cells;
+
   constructor(gridElement) {
     gridElement.style.setProperty("--grid-size", GRID_SIZE);
     gridElement.style.setProperty("--cell-size", `${CELL_SIZE}vmin`);
@@ -15,6 +16,26 @@ export default class Grid {
         Math.floor(index / GRID_SIZE)
       );
     });
+  }
+
+  get cells() {
+    return this.#cells;
+  }
+
+  get cellsByColumn() {
+    return this.#cells.reduce((cellGrid, cell) => {
+      cellGrid[cell.x] = cellGrid[cell.x] || [];
+      cellGrid[cell.x][cell.y] = cell;
+      return cellGrid;
+    }, []);
+  }
+
+  get cellsByRow() {
+    return this.#cells.reduce((cellGrid, cell) => {
+      cellGrid[cell.y] = cellGrid[cell.y] || [];
+      cellGrid[cell.y][cell.x] = cell;
+      return cellGrid;
+    }, []);
   }
 
   get #emptyCells() {
@@ -31,11 +52,19 @@ class Cell {
   #x;
   #y;
   #tile;
+  #mergeTile;
 
   constructor(cellElement, x, y) {
     this.#cellElement = cellElement;
     this.#x = x;
     this.#y = y;
+  }
+
+  get x() {
+    return this.#x;
+  }
+  get y() {
+    return this.#y;
   }
 
   get tile() {
@@ -47,9 +76,37 @@ class Cell {
     if (value == null) {
       return;
     }
-
     this.#tile.x = this.#x;
     this.#tile.y = this.#y;
+  }
+
+  get mergeTile() {
+    return this.#mergeTile;
+  }
+
+  set mergeTile(value) {
+    this.#mergeTile = value;
+    if (value == null) {
+      return;
+    }
+    this.#mergeTile.x = this.#x;
+    this.#mergeTile.y = this.#y;
+  }
+
+  canAccept(tile) {
+    return (
+      this.tile == null ||
+      (this.mergeTile == null && this.tile.value == tile.value)
+    );
+  }
+
+  mergeTiles() {
+    if (this.tile == null || this.mergeTile == null) {
+      return;
+    }
+    this.tile.value = this.tile.value + this.mergeTile.value;
+    this.mergeTile.remove();
+    this.mergeTile = null;
   }
 }
 
--- Tile.js
+++ Tile.js
@@ -11,6 +11,10 @@ export default class Tile {
     this.value = value;
   }
 
+  get value() {
+    return this.#value;
+  }
+
   set value(v) {
     this.#value = v;
     this.#tileElement.textContent = v;
@@ -34,5 +38,9 @@ export default class Tile {
   set y(value) {
     this.#y = value;
     this.#tileElement.style.setProperty("--y", value);
+  }
+
+  remove() {
+    this.#tileElement.remove();
   }
 }
